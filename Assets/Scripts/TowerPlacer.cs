e692e948d4b23701cf7d7ae214dbc38dca551d6d
Reployerr <166069908+Reployerr@users.noreply.github.com>
2024-04-23 17:45:33 +0300

    drag&place ver 0.2
    Не работают нормально рейкасты с коллайдерами. Сделать отдельную систему состояний башен при поиске расположения

--- Assets/Scripts/TowerPlacer.cs
+++ Assets/Scripts/TowerPlacer.cs
@@ -6,9 +6,13 @@ using UnityEngine.EventSystems;
 public class TowerPlacer : MonoBehaviour
 {
     public LayerMask groundLayerMask;
+    public LayerMask roadLayerMask;
 
     private GameObject _buildingPrefab;
     private GameObject _toBuild;
+    private SpriteRenderer prefabSprite;
+     
+    
 
     private Camera _mainCamera;
 
@@ -20,6 +24,7 @@ public class TowerPlacer : MonoBehaviour
 
     private void Update()
     {
+        
         if (_buildingPrefab != null)
         {
             if (EventSystem.current.IsPointerOverGameObject())
@@ -31,14 +36,24 @@ public class TowerPlacer : MonoBehaviour
 
             Vector3 mouseWorldPosition = _mainCamera.ScreenToWorldPoint(Input.mousePosition);
             mouseWorldPosition.z = 0f;
-            if (Physics2D.Raycast(mouseWorldPosition, mouseWorldPosition, 1000f, groundLayerMask))
+
+            if (Physics2D.Raycast(mouseWorldPosition, _mainCamera.transform.position, 0.3f, groundLayerMask))
             {
+                prefabSprite.color = Color.white;
+                Debug.DrawRay(mouseWorldPosition, _mainCamera.transform.position, Color.red);
                 if (!_toBuild.activeSelf) _toBuild.SetActive(true);
                 Debug.Log("groundLayer");
                 _toBuild.transform.position = mouseWorldPosition;
+                
             }
             else if (_toBuild.activeSelf) _toBuild.SetActive(false);
 
+            if(Physics2D.Raycast(mouseWorldPosition, _mainCamera.transform.position, 0.3f, roadLayerMask))
+            {
+                Debug.Log("road triggered");
+                prefabSprite.color = Color.red;
+                
+            }
         }  
 
     }
@@ -46,14 +61,17 @@ public class TowerPlacer : MonoBehaviour
     public void SetBuildingPrefab(GameObject prefab)
     {
         _buildingPrefab = prefab;
+        prefabSprite = _buildingPrefab.GetComponent<SpriteRenderer>();
         PrepareBuilding();
     }
 
     private void PrepareBuilding()
     {
+
         if (_toBuild) Destroy(_toBuild);
 
         _toBuild = Instantiate(_buildingPrefab);
         _toBuild.SetActive(false);
+        prefabSprite.color = Color.white;
     }
 }

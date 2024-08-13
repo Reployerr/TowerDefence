41dd85e27efc6e7f5a08b39c327c7d24ca6a4155
Reployerr <166069908+Reployerr@users.noreply.github.com>
2024-08-13 17:36:02 +0300

    Added second enemy, wave spawner updated

--- Assets/Scripts/WaveSystem/WaveSpawner.cs
+++ Assets/Scripts/WaveSystem/WaveSpawner.cs
@@ -12,15 +12,18 @@ public class WaveSpawner : MonoBehaviour
 	[SerializeField] private float _enemiesPerSecond = 0.5f;
 	[SerializeField] private float _timeBetweenWaves = 5f; // time before new wave starts
 	[SerializeField] private float difficultyScalingFactor = 0.75f;
+	[SerializeField] private float enemiesPerSecondCap = 15f;
 
 	[Header("Events")]
 	public static UnityEvent onEnemyDestroy = new UnityEvent();
 
-	private int _currentWave = 1;
+	public int _currentWave = 1;
+
 	private float _timeSinceLastSpawn; // time after last enemy's wave has spawned
 	private int _enemiesAlive;
 	private int _enemiesLeftToSpawn; // осталось заспавнить
 	private bool isSpawning;
+	private float eps; //enemies per second
 
 	private void Awake()
 	{
@@ -38,7 +41,7 @@ public class WaveSpawner : MonoBehaviour
 
 		_timeSinceLastSpawn += Time.deltaTime;
 
-		if(_timeSinceLastSpawn >= (1f / _enemiesPerSecond) && _enemiesLeftToSpawn > 0)
+		if(_timeSinceLastSpawn >= (1f / eps) && _enemiesLeftToSpawn > 0)
 		{
 			SpawnEnemy();
 			_enemiesLeftToSpawn--;
@@ -64,6 +67,7 @@ public class WaveSpawner : MonoBehaviour
 
 		isSpawning = true;
 		_enemiesLeftToSpawn = EnemiesPerWave();
+		eps = EnemiesPerSecond();
 	}
 
 	private void EndWave()
@@ -77,13 +81,41 @@ public class WaveSpawner : MonoBehaviour
 	private void SpawnEnemy()
 	{
 		Debug.Log("Enemy spawned");
-		GameObject prefabToSpawn = enemyPrefabs[0];
-		Instantiate(prefabToSpawn, LevelManager.main.startPoint.position, Quaternion.identity);
+		
+		if(_currentWave < 4)
+		{
+			GameObject prefabToSpawn = enemyPrefabs[0];
+			Instantiate(prefabToSpawn, LevelManager.main.startPoint.position, Quaternion.identity);
+		}
 
+		if(_currentWave == 4)
+		{
+			/*GameObject prefabToSpawnImp = enemyPrefabs[1];
+
+			for (int i = 0; i < _baseEnemies; i++)
+			{
+				
+				Instantiate(prefabToSpawnImp, LevelManager.main.startPoint.position, Quaternion.identity);
+			}*/
+			GameObject prefabToSpawn = enemyPrefabs[1];
+			Instantiate(prefabToSpawn, LevelManager.main.startPoint.position, Quaternion.identity);
+		}
+
+		if(_currentWave > 4)
+		{
+			int index = Random.Range(0, enemyPrefabs.Length);
+			GameObject prefabToSpawn = enemyPrefabs[index];
+			Instantiate(prefabToSpawn, LevelManager.main.startPoint.position, Quaternion.identity);
+		}
 	}
 
 	private int EnemiesPerWave()
 	{
 		return Mathf.RoundToInt(_baseEnemies * Mathf.Pow(_currentWave, difficultyScalingFactor)); // умножаем базовое количество врагов на текущую волну возведя  в степень показателя сложности
+	}
+
+	private float EnemiesPerSecond()
+	{
+		return Mathf.Clamp(_enemiesPerSecond * Mathf.Pow(_currentWave, difficultyScalingFactor), 0, enemiesPerSecondCap); // умножаем базовое количество врагов на текущую волну возведя  в степень показателя сложности
 	}
 }
